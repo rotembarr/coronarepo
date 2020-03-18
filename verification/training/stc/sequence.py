a8279afc8f480dec41fbd4ce311768f4c7677551
Roy Tenenbaum <roy.tenenbaum@gmail.com>
2020-03-18 16:07:26 +0200

    Minor fixes

--- verification/training/stc/sequence.py
+++ verification/training/stc/sequence.py
@@ -4,9 +4,11 @@ class Sequence:
         self.rm_list = []
 
     def run(self, num_of_messages):
-        for i in num_of_messages:
+        for i in range(num_of_messages):
             # gen msg
             # TODO add implementation
             msg = ''
             self.dut_list.append(msg)
             self.rm_list.append(msg)
+
+        print("Finished sequence")
