300488df7c168fd94605eb0b8e68ef5c1f9b65b4
NeriyaMinerbi <neriyam1@gmail.com>
2020-03-19 22:54:15 +0200

    Added percentage function

--- verification/training/stc/package.py
+++ verification/training/stc/package.py
@@ -1,3 +1,5 @@
+import random
+
 NUM_OF_MSG = 3
 
 MINUTE_IN_SECONDS = 60
@@ -11,6 +13,25 @@ SYNC_1 = {'sync': 'bb', 'payload': 2}
 SYNC_2 = {'sync': 'cc', 'payload': 3}
 SYNC_3 = {'sync': 'dd', 'payload': 4}
 SYNCS = [SYNC_0, SYNC_1, SYNC_2, SYNC_3]
+
+
+def is_true_by_percentage(percent, max_percent=100, min_percent=1):
+    """
+    The function received the number and checks if the number is greater than the random number we produce(in the range)
+
+    :param percent: The user number that indicate the percentage to return True from the max percent.
+    :param min_percent: The min percent on the range.
+    :param max_percent: The max percent on the range.
+    :return: True if the percent bigger then the random number else False.
+    """
+
+    # Check if the user percent bigger then the max range.
+    assert percent <= max_percent
+
+    # Generate random number between the range.
+    random_num = random.randrange(min_percent, max_percent)
+
+    return True if percent > random_num else False
 
 
 # define Python user-defined exceptions
@@ -27,3 +48,7 @@ class ComparisionFailed(Error):
 class TimeoutOccurred(Error):
     """Raised When The Timeout Reached His Limit"""
     pass
+
+
+if __name__ == "__main__":
+    print(is_true_by_percentage(100))
