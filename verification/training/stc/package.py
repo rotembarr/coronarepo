14307b5069c3164984f060e5412b55d1127a396e
OranOved <46324844+OranOved@users.noreply.github.com>
2020-03-22 20:08:03 +0200

    updates environment and package

--- verification/training/stc/package.py
+++ verification/training/stc/package.py
@@ -1,42 +1,24 @@
 import random
 
 # Simulation parameters
-NUM_OF_BYTES_TO_SEND = 3000
-FULL_PERCENTAGE = 100
+NUM_OF_BYTES_TO_SEND = 1000
 
 # DUT parameters
-BUS_WIDTH_IN = 1
+BUS_WIDTH_IN_BYTES = 1
 
 # Sequence
-GEN_GOOD_SYNC_P = 20
-GEN_RAND_SYNC_PROB = 50
-RAND_SYNC_MIN_SIZE = BUS_WIDTH_IN
-RAND_SYNC_MAX_SIZE = BUS_WIDTH_IN * 3
+RAND_VALUE = '@'
+RAND_BYTE_W = 1
+RAND_BYTE_IN_SYNC_0_W = 30
+RAND_BYTE_IN_SYNC_1_W = 0
+RAND_BYTE_IN_SYNC_PARAM_W = 0
 
 # Payload in bytes
 # The sync series is in hexadecimal
-PARAM_SYNC = {'sync': 'c', 'payload': 10}
-SYNCS = [{'sync': 'AABB0034', 'payload': 55}, 
+PARAM_SYNC = {'sync': 'ads', 'payload': 10}
+SYNCS = [{'sync': 'AABB0034', 'payload': 55},
          {'sync': '34569865543', 'payload': 124},
          PARAM_SYNC]
-
-def is_true_by_percentage(percent, max_percent=100, min_percent=1):
-    """
-    The function received the number and checks if the number is greater than the random number we produce(in the range)
-
-    :param percent: The user number that indicate the percentage to return True from the max percent.
-    :param min_percent: The min percent on the range.
-    :param max_percent: The max percent on the range.
-    :return: True if the percent bigger then the random number else False.
-    """
-
-    # Check if the user percent bigger then the max range.
-    assert percent <= max_percent
-
-    # Generate random number between the range.
-    random_num = random.randrange(min_percent, max_percent)
-
-    return True if percent > random_num else False
 
 
 # define Python user-defined exceptions
@@ -48,12 +30,3 @@ class Error(Exception):
 class ComparisionFailed(Error):
     """Raised When The Comparision In The Scoreboard Has Failed"""
     pass
-
-
-class TimeoutOccurred(Error):
-    """Raised When The Timeout Reached His Limit"""
-    pass
-
-
-if __name__ == "__main__":
-    print(is_true_by_percentage(100))
