324f6873cf1cac25555cd037d762c45852cffbdf
BarHaran <barharan222@gmail.com>
2020-04-28 19:45:47 +0300

    Add placeholders

---
+++ loopback/rtl/registers_controller.sv
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////
+// This module's purpose is to allow future  //
+// debug access using the registers. 		 //
+///////////////////////////////////////////////
+
+module registers_controller 
+#(
+	parameter int COUNTER_SIZE = 32
+)
+(
+	// General
+	input logic clk,
+	input logic rst_n,
+	
+	// Avalon-mm
+);
+
+endmodule
