59499fce3317b783f868805f6a9f36b846f5d800
Bar Haran <barharan222@gmail.com>
2020-05-06 15:30:23 +0300

    Update some more

--- led_blinker/rtl/led_blinker_controller.sv
+++ led_blinker/rtl/led_blinker_controller.sv
@@ -1,3 +1,5 @@
+// Each address should be considered as {0x000004}XX
+
 module led_blinker_controller (
 	input logic clk,
 	input logic rst,
@@ -8,16 +10,19 @@ module led_blinker_controller (
 	input 	logic 			master_mm_write,
 	input 	logic [31:0] 	master_mm_writedata,
 	output 	logic 			master_mm_readdatavalid,
-	input 	logic 			master_rst,
 	output 	logic 			master_mm_waitrequest,
+	input 	logic 			master_rst,
 
-	output logic led_active
+	input	logic msg_enter,
+	output 	logic led_active
 );
 
 logic [31:0] test_reg;
 
 logic cntr_active;
 logic [25:0] blink_counter;
+
+logic [31:0] msg_counter;
 
 always_ff @(posedge clk or negedge rst) begin : proc_sync
 	if(~rst) begin
@@ -30,32 +35,51 @@ always_ff @(posedge clk or negedge rst) begin : proc_sync
 		cntr_active 			<= 0;
 	end else begin
 		master_mm_waitrequest  	<= master_mm_write & ~master_mm_waitrequest;
+		// Write by address
 		if (master_mm_write) begin
 			if (master_mm_address == 0) begin
+				// Only one bit to activate counter
 				cntr_active <= master_mm_writedata[0];
 			end else if (master_mm_address == 4) begin
+				// Test register, has no usage but to test writing and reading
 				test_reg <= master_mm_writedata;
 			end
 		end
 
+		// Increase counter if defined
 		if (cntr_active) begin
 			blink_counter <= blink_counter + 1;
 		end else begin
+			// If not active, resets the counter in order to prevent a case when counter stuck on only 1
 			blink_counter <= 0;
 		end
 
+		// When counter reaches only 111..., de/activates the led
 		if (blink_counter == {26{1'b1}}) begin
 			led_active <= ~led_active;
 		end
 
+		// Increase msg counter
+		if (msg_enter) begin
+			msg_counter <= msg_counter + 1'b1;
+		end
+
+		// Reset the sent data and valid if no request
 		master_mm_readdata 		<= 0;
 		master_mm_readdatavalid <= 1'b0;
+		// Read request
 		if (master_mm_read) begin
+			// Active led blinker
 			if (master_mm_address == 0) begin
 				master_mm_readdata 		<= {{31{1'b0}}, cntr_active};
 				master_mm_readdatavalid <= 1'b1;
+			// Test register
 			end else if (master_mm_address == 4) begin
 				master_mm_readdata 		<= test_reg;
+				master_mm_readdatavalid <= 1'b1;
+			// Msg counter
+			end else if (master_mm_address == 8) begin
+				master_mm_readdata 		<= msg_counter;
 				master_mm_readdatavalid <= 1'b1;
 			end
 		end
--- led_blinker/rtl/led_blinker_top.v
+++ led_blinker/rtl/led_blinker_top.v
@@ -23,11 +23,11 @@ module led_blinker_top
 	// NET_SGMII_RX,
 	// NET_SGMII_TX,
 	PB_RESETn,
-	// PC_PHY_MDC,
-	// PC_PHY_MDIO,
-	// PC_PHY_RESETn,
-	// PC_SGMII_RX,
-	// PC_SGMII_TX,
+	PC_PHY_MDC,
+	PC_PHY_MDIO,
+	PC_PHY_RESETn,
+	PC_SGMII_RX,
+	PC_SGMII_TX,
 	PLL_LOCKED,
 	XCVR_REFCLK_125
 // {ALTERA_ARGS_END} DO NOT REMOVE THIS LINE!
@@ -41,62 +41,116 @@ module led_blinker_top
 // input			NET_SGMII_RX;
 // output			NET_SGMII_TX;
 input			PB_RESETn;
-// output			PC_PHY_MDC;
-// inout			PC_PHY_MDIO;
-// output			PC_PHY_RESETn;
-// input			PC_SGMII_RX;
-// output			PC_SGMII_TX;
+output			PC_PHY_MDC;
+inout			PC_PHY_MDIO;
+output			PC_PHY_RESETn;
+input			PC_SGMII_RX;
+output			PC_SGMII_TX;
 output			PLL_LOCKED;
 input			XCVR_REFCLK_125;
 
 // {ALTERA_IO_END} DO NOT REMOVE THIS LINE!
 // {ALTERA_MODULE_BEGIN} DO NOT REMOVE THIS LINE!
 
-
-	led_blinker_controller i_led_blinker_controller (
-		.clk                    (pll_0_outclk0_clk        ),
-		.rst                    (PB_RESETn              ),
-		.master_mm_address      (master_mm_address      ),
-		.master_mm_readdata     (master_mm_readdata     ),
-		.master_mm_read         (master_mm_read         ),
-		.master_mm_write        (master_mm_write        ),
-		.master_mm_writedata    (master_mm_writedata    ),
-		.master_mm_readdatavalid(master_mm_readdatavalid),
-		.master_rst             (master_rst_reset       ),
-		.led_active             (PLL_LOCKED             ),
-		.master_mm_waitrequest  (master_mm_waitrequest  )
-	);
-
-	wire [31:0] master_mm_address;
-	wire [31:0] master_mm_readdata;
-	wire master_mm_read;
-	wire master_mm_write;
-	wire [31:0] master_mm_writedata;
-	wire master_mm_readdatavalid;
+	// Qsys signals
+	wire eth_mdio_mdio_in;
+	wire eth_mdio_mdio_out;
+	wire eth_mdio_mdio_oen;
+	wire [31:0] eth_recieve_data;
+	wire eth_recieve_endofpacket;
+	wire [1:0] eth_recieve_empty;
+	wire eth_recieve_ready;
+	wire eth_recieve_startofpacket;
+	wire eth_recieve_valid;
+	wire [31:0] eth_transmit_data;
+	wire eth_transmit_endofpacket;
+	wire [1:0] eth_transmit_empty;
+	wire eth_transmit_ready;
+	wire eth_transmit_startofpacket;
+	wire eth_transmit_valid;
 	wire master_rst_reset;
-	wire master_mm_waitrequest;
-	wire master_mm_byteenable;
-
-
-	wire pll_0_outclk0_clk;
-	wire pll_0_refclk_clk;
+	wire pll_clk_clk;
+	wire [31:0] reg_mm_readdata;
+	wire reg_mm_readdatavalid;
+	wire [31:0] reg_mm_writedata;
+	wire reg_mm_waitrequest;
+	wire [9:0] reg_mm_address;
+	wire reg_mm_write;
+	wire reg_mm_read;
 
 led_blinker i_led_blinker (
-	.clk_clk                (pll_0_outclk0_clk),
-	.master_mm_address      (master_mm_address      ),
-	.master_mm_readdata     (master_mm_readdata     ),
-	.master_mm_read         (master_mm_read         ),
-	.master_mm_write        (master_mm_write        ),
-	.master_mm_writedata    (master_mm_writedata    ),
-	.master_mm_waitrequest  (master_mm_waitrequest  ),
-	.master_mm_readdatavalid(master_mm_readdatavalid),
-	.master_mm_byteenable   (master_mm_byteenable   ), // TODO: Check connection ! Signal/port not matching : Expecting logic [3:0]  -- Found logic 
-	.master_rst_reset       (master_rst_reset       ),
-	.pll_0_outclk0_clk      (pll_0_outclk0_clk      ),
-	.pll_0_refclk_clk       (XCVR_REFCLK_125       ),
-	.rst_reset_n            (PB_RESETn            )
+	.clk_clk                   (XCVR_REFCLK_125),
+	.eth_mdio_mdc              (PC_PHY_MDC),
+	.eth_mdio_mdio_in          (eth_mdio_mdio_in),
+	.eth_mdio_mdio_out         (eth_mdio_mdio_out),
+	.eth_mdio_mdio_oen         (eth_mdio_mdio_oen),
+	.eth_recieve_data          (eth_recieve_data),
+	.eth_recieve_endofpacket   (eth_recieve_endofpacket),
+	.eth_recieve_error         (),
+	.eth_recieve_empty         (eth_recieve_empty),
+	.eth_recieve_ready         (eth_recieve_ready),
+	.eth_recieve_startofpacket (eth_recieve_startofpacket),
+	.eth_recieve_valid         (eth_recieve_valid),
+	.eth_sgmii_rxp             (PC_SGMII_RX),
+	.eth_sgmii_txp             (PC_SGMII_TX),
+	.eth_transmit_data         (eth_transmit_data),
+	.eth_transmit_endofpacket  (eth_transmit_endofpacket),
+	.eth_transmit_error        (),
+	.eth_transmit_empty        (eth_transmit_empty),
+	.eth_transmit_ready        (eth_transmit_ready),
+	.eth_transmit_startofpacket(eth_transmit_startofpacket),
+	.eth_transmit_valid        (eth_transmit_valid),
+	.fifo_in_data              (eth_recieve_data),
+	.fifo_in_valid             (eth_recieve_valid),
+	.fifo_in_ready             (eth_recieve_ready),
+	.fifo_in_startofpacket     (eth_recieve_startofpacket),
+	.fifo_in_endofpacket       (eth_recieve_endofpacket),
+	.fifo_in_empty             (eth_recieve_empty),
+	.fifo_out_data             (eth_transmit_data),
+	.fifo_out_valid            (eth_transmit_valid),
+	.fifo_out_ready            (eth_transmit_ready),
+	.fifo_out_startofpacket    (eth_transmit_startofpacket),
+	.fifo_out_endofpacket      (eth_transmit_endofpacket),
+	.fifo_out_empty            (eth_transmit_empty),
+	.master_rst_reset          (master_rst_reset),
+	.pll_clk_clk               (pll_clk_clk),
+	.ref_clk_clk               (XCVR_REFCLK_125),
+	.reg_mm_waitrequest        (reg_mm_waitrequest),
+	.reg_mm_readdata           (reg_mm_readdata),
+	.reg_mm_readdatavalid      (reg_mm_readdatavalid),
+	.reg_mm_burstcount         (),
+	.reg_mm_writedata          (reg_mm_writedata),
+	.reg_mm_address            (reg_mm_address),
+	.reg_mm_write              (reg_mm_write),
+	.reg_mm_read               (reg_mm_read),
+	.reg_mm_byteenable         (),
+	.reg_mm_debugaccess        (),
+	.rst_reset_n               (PB_RESETn)
 );
 
+assign PC_PHY_MDIO 		= (eth_mdio_mdio_oen) ? 1'bz : eth_mdio_mdio_out;
+assign eth_mdio_mdio_in = PC_PHY_MDIO;
+
+assign PC_PHY_RESETn 	= PB_RESETn;
+
+	wire msg_enter;
+
+assign msg_enter = eth_recieve_ready & eth_recieve_endofpacket & eth_recieve_valid;
+
+led_blinker_controller i_led_blinker_controller (
+	.clk 					(pll_clk_clk),
+	.rst 					(PB_RESETn),
+	.master_mm_address      (reg_mm_address),
+	.master_mm_readdata     (reg_mm_readdata),
+	.master_mm_read         (reg_mm_read),
+	.master_mm_write        (reg_mm_write),
+	.master_mm_writedata    (reg_mm_writedata),
+	.master_mm_readdatavalid(reg_mm_readdatavalid),
+	.master_mm_waitrequest  (reg_mm_waitrequest),
+	.master_rst             (master_rst_reset),
+	.led_active             (PLL_LOCKED),
+	.msg_enter 				(msg_enter)
+);
 
 
 // {ALTERA_MODULE_END} DO NOT REMOVE THIS LINE!
