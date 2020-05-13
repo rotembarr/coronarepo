6cf33f787692b4ef13b1eff51679be1390c9f923
Ofir Shabat <ofirshabat11@gmail.com>
2020-05-13 14:42:14 +0300

    The client is not waiting for any messages from the server now

--- udp/client_w.c
+++ udp/client_w.c
@@ -8,6 +8,9 @@
 // The compilation line is:
 // gcc client_w.c -o client -g -O0 -Wall -msse2 -msse -march=native -maes -l ws2_32
 
+// Defines
+#define SIO_UDP_CONNRESET _WSAIOW(IOC_VENDOR, 12)
+
 int main()
 {
 	struct sockaddr_in server_addr;
@@ -16,13 +19,12 @@ int main()
 	int port_num;
 	WSADATA wsa;
 
+	// Variables for disabling the error that occurs while waiting for the server to seem to be connected
+	BOOL bNewBehavior = FALSE;
+	DWORD dwBytesReturned = 0;
+	
 	// This string will hold the key for the AES encryption
 	char* enc_key_str = "1111111111111111\0";
-
-	// A variable for the buffer to receive data from the server 
-	char* buff;
-	buff = (char*) malloc(BUFF_LEN*sizeof(char));
-	memset(buff, '\0', BUFF_LEN);
 
 	// A variable for the data to get from the user to send to the server
 	char* message;
@@ -33,11 +35,6 @@ int main()
 	char* message_enc;
 	message_enc = (char*) malloc(BUFF_LEN*sizeof(char));
 	memset(message_enc, '\0', BUFF_LEN);
-
-	// A variable for the buffer to receive data from the server 
-	char* message_dec;
-	message_dec = (char*) malloc(BUFF_LEN*sizeof(char));
-	memset(message_dec, '\0', BUFF_LEN);
 
 	// The size of server_addr struct
 	slen = sizeof(server_addr);
@@ -71,6 +68,9 @@ int main()
 		printf("socket() failed with error code : %d" , WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
+
+	// Disable the error that occurs while waiting for the server to seem to be connected
+	WSAIoctl(socket_id, SIO_UDP_CONNRESET, &bNewBehavior, sizeof(bNewBehavior), NULL, 0, &dwBytesReturned, NULL, NULL);
 	
 	// Setup address structure
 	memset((char *) &server_addr, 0, sizeof(server_addr));
@@ -92,19 +92,6 @@ int main()
 	enc_message_aux = (uint8_t*) malloc(BUFF_LEN*sizeof(uint8_t));
 	memset(enc_message_aux, '\0', BUFF_LEN);
 
-	// Auxiliary variables for the decryption process (data from the server)
-	// data_to_dec will hold each 16 bytes to decrypt for each encryption process
-	char* data_to_dec;
-	data_to_dec = (char*) malloc(17*sizeof(char));
-	memset(data_to_dec, '\0', 17);
-
-	// This varaible will point to the encrypted data in uint8_t type
-	uint8_t* aux_dec;
-
-	// Will hold the whole message encrypted in uint8_t type
-	uint8_t* dec_message_aux;
-	dec_message_aux = (uint8_t*) malloc(BUFF_LEN*sizeof(uint8_t));
-	memset(dec_message_aux, '\0', BUFF_LEN);
 
 	// Start the communication
 	while(strcmp("exit", message) & strcmp("quit", message))
@@ -147,61 +134,13 @@ int main()
 			exit(EXIT_FAILURE);
 		}
 		
-		// Receive a reply and print it
-		// Clear the buffer by filling null, it might have previously received data
-		memset(buff,'\0', BUFF_LEN);
-
-		// Try to receive some data, this is a blocking call
-		if (recvfrom(socket_id, buff, BUFF_LEN, 0, (struct sockaddr *) &server_addr, &slen) == SOCKET_ERROR)
-		{
-			printf("recvfrom() failed with error code : %d" , WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
-
-		printf("Data encrypted: %s\n" , buff);
-
-		// Decrypt the message
-		// Number of 16-bytes iterartions needed for the AES process
-		int num_decryptions = (int) ceil((double)strlen(buff)/16.0);
-
-		// Setting the memory
-		memset(data_to_dec, 	'\0', 17);
-		memset(message_dec, 	'\0', BUFF_LEN);
-		memset(dec_message_aux, '\0', BUFF_LEN);
-
-		// Decrypt for each process of 16-byte
-		for (int i = 0; i < num_decryptions; i++)
-		{
-			memcpy(data_to_dec, buff+16*i*sizeof(char), 16*sizeof(char));
-			data_to_dec[16] = '\0';
-			aux_dec = aes_dec_128bits(data_to_dec, enc_key_str);
-			// Check if the decryption failed
-			if (aux_dec == NULL){
-				return 1;
-			}
-			memcpy(dec_message_aux+16*i*sizeof(char), aux_dec, 16*sizeof(uint8_t));
-			free(aux_dec);
-		}
-
-		// Casting the message to chars
-		for (int i = 0; i < num_decryptions*16+1; i++)
-    	{
-        	message_dec[i] = (char)dec_message_aux[i];
-    	}
-		
-		// Print the received data
-		puts(message_dec);
 	}
 
 	// Free the memory
-	free(buff);
 	free(message);
 	free(message_enc);
-	free(message_dec);
 	free(data_to_enc);
-	free(data_to_dec);
 	free(enc_message_aux);
-	free(dec_message_aux);
 
 	// Close the socket
 	closesocket(socket_id);
