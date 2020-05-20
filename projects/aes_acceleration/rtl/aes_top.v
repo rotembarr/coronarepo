9db66de0a68a256a79b6b692be674926ce4edf6c
Bar Haran <barharan222@gmail.com>
2020-05-20 18:08:35 +0300

    Add FIFO to prevent trouble

--- projects/aes_acceleration/rtl/aes_top.v
+++ projects/aes_acceleration/rtl/aes_top.v
@@ -52,25 +52,11 @@ output			PC_PHY_RESETn;
 assign NET_PHY_RESETn = PB_RESETn;
 assign PC_PHY_RESETn  = PB_RESETn;
 
-// Connections from QSYS to top module
+
+	wire clk_clk;
 	wire eth_net_mac_mdio_mdio_in;
 	wire eth_net_mac_mdio_mdio_out;
 	wire eth_net_mac_mdio_mdio_oen;
-	wire [31:0] eth_net_recieve_data;
-	wire eth_net_recieve_endofpacket;
-	wire [5:0] eth_net_recieve_error;
-	wire [1:0] eth_net_recieve_empty;
-	wire eth_net_recieve_ready;
-	wire eth_net_recieve_startofpacket;
-	wire eth_net_recieve_valid;
-	wire [31:0] eth_net_transmit_data;
-	wire eth_net_transmit_endofpacket;
-	wire eth_net_transmit_error;
-	wire [1:0] eth_net_transmit_empty;
-	wire eth_net_transmit_ready;
-	wire eth_net_transmit_startofpacket;
-	wire eth_net_transmit_valid;
-	wire master_rst_reset;
 	wire mm_out_waitrequest;
 	wire [31:0] mm_out_readdata;
 	wire mm_out_readdatavalid;
@@ -78,75 +64,82 @@ assign PC_PHY_RESETn  = PB_RESETn;
 	wire [9:0] mm_out_address;
 	wire mm_out_write;
 	wire mm_out_read;
-	wire clk;
-
-
+	wire [31:0] recieve_out_data;
+	wire recieve_out_valid;
+	wire recieve_out_ready;
+	wire recieve_out_startofpacket;
+	wire recieve_out_endofpacket;
+	wire [1:0] recieve_out_empty;
+	wire [31:0] transmit_in_data;
+	wire transmit_in_valid;
+	wire transmit_in_ready;
+	wire transmit_in_startofpacket;
+	wire transmit_in_endofpacket;
+	wire [1:0] transmit_in_empty;
 one_mac i_one_mac (
-	.eth_net_conn_rxp              (NET_SGMII_RX                  ),
-	.eth_net_conn_txp              (NET_SGMII_TX                  ),
-	.eth_net_mac_mdio_mdc          (NET_PHY_MDC                   ),
-	.eth_net_mac_mdio_mdio_in      (eth_net_mac_mdio_mdio_in      ),
-	.eth_net_mac_mdio_mdio_out     (eth_net_mac_mdio_mdio_out     ),
-	.eth_net_mac_mdio_mdio_oen     (eth_net_mac_mdio_mdio_oen     ),
-	.eth_net_recieve_data          (eth_net_recieve_data          ),
-	.eth_net_recieve_endofpacket   (eth_net_recieve_endofpacket   ),
-	.eth_net_recieve_error         (eth_net_recieve_error         ),
-	.eth_net_recieve_empty         (eth_net_recieve_empty         ),
-	.eth_net_recieve_ready         (eth_net_recieve_ready         ),
-	.eth_net_recieve_startofpacket (eth_net_recieve_startofpacket ),
-	.eth_net_recieve_valid         (eth_net_recieve_valid         ),
-	.eth_net_transmit_data         (eth_net_transmit_data         ),
-	.eth_net_transmit_endofpacket  (eth_net_transmit_endofpacket  ),
-	.eth_net_transmit_error        (eth_net_transmit_error        ),
-	.eth_net_transmit_empty        (eth_net_transmit_empty        ),
-	.eth_net_transmit_ready        (eth_net_transmit_ready        ),
-	.eth_net_transmit_startofpacket(eth_net_transmit_startofpacket),
-	.eth_net_transmit_valid        (eth_net_transmit_valid        ),
-	.master_rst_reset              (master_rst_reset              ),
-	.mm_out_waitrequest            (mm_out_waitrequest            ),
-	.mm_out_readdata               (mm_out_readdata               ),
-	.mm_out_readdatavalid          (mm_out_readdatavalid          ),
-	.mm_out_burstcount             (                              ),
-	.mm_out_writedata              (mm_out_writedata              ),
-	.mm_out_address                (mm_out_address                ),
-	.mm_out_write                  (mm_out_write                  ),
-	.mm_out_read                   (mm_out_read                   ),
-	.mm_out_byteenable             (                              ),
-	.mm_out_debugaccess            (                              ),
-	.reset_reset_n                 (PB_RESETn                     ),
-	.out_pll_outclk0_clk           (clk                           ),
-	.out_pll_refclk_clk            (XCVR_REFCLK_125               ),
-	.clk_clk                       (XCVR_REFCLK_125)
+	.clk_clk                  (XCVR_REFCLK_125          ),
+	.eth_net_conn_rxp         (NET_SGMII_RX             ),
+	.eth_net_conn_txp         (NET_SGMII_TX             ),
+	.eth_net_mac_mdio_mdc     (NET_PHY_MDC              ),
+	.eth_net_mac_mdio_mdio_in (eth_net_mac_mdio_mdio_in ),
+	.eth_net_mac_mdio_mdio_out(eth_net_mac_mdio_mdio_out),
+	.eth_net_mac_mdio_mdio_oen(eth_net_mac_mdio_mdio_oen),
+	.master_rst_reset         (                         ),
+	.mm_out_waitrequest       (mm_out_waitrequest       ),
+	.mm_out_readdata          (mm_out_readdata          ),
+	.mm_out_readdatavalid     (mm_out_readdatavalid     ),
+	.mm_out_burstcount        (                         ),
+	.mm_out_writedata         (mm_out_writedata         ),
+	.mm_out_address           (mm_out_address           ),
+	.mm_out_write             (mm_out_write             ),
+	.mm_out_read              (mm_out_read              ),
+	.mm_out_byteenable        (                         ),
+	.mm_out_debugaccess       (                         ),
+	.out_pll_outclk0_clk      (clk_clk                  ),
+	.out_pll_refclk_clk       (XCVR_REFCLK_125          ),
+	.recieve_out_data         (recieve_out_data         ),
+	.recieve_out_valid        (recieve_out_valid        ),
+	.recieve_out_ready        (recieve_out_ready        ),
+	.recieve_out_startofpacket(recieve_out_startofpacket),
+	.recieve_out_endofpacket  (recieve_out_endofpacket  ),
+	.recieve_out_empty        (recieve_out_empty        ),
+	.reset_reset_n            (PB_RESETn                ),
+	.transmit_in_data         (transmit_in_data         ),
+	.transmit_in_valid        (transmit_in_valid        ),
+	.transmit_in_ready        (transmit_in_ready        ),
+	.transmit_in_startofpacket(transmit_in_startofpacket),
+	.transmit_in_endofpacket  (transmit_in_endofpacket  ),
+	.transmit_in_empty        (transmit_in_empty        )
 );
-
 
 	assign eth_net_mac_mdio_mdio_in = NET_PHY_MDIO;
 	assign NET_PHY_MDIO = (eth_net_mac_mdio_mdio_oen == 1'b0) ? eth_net_mac_mdio_mdio_out : 1'bz;
-
+	
 aes_sv_top i_aes_sv_top (
-	.clk                    (XCVR_REFCLK_125),
-	.rst_n                  (PB_RESETn),
-	.mm_master_address      (mm_out_address),
-	.mm_master_write        (mm_out_write),
-	.mm_master_writedata    (mm_out_writedata),
-	.mm_master_read         (mm_out_read),
-	.mm_master_readdatavalid(mm_out_readdatavalid),
-	.mm_master_readdata     (mm_out_readdata),
-	.mm_master_waitrequest  (mm_out_waitrequest),
-	.debug_led              (PLL_LOCKED),
-	.recieve_st_data        (eth_net_recieve_data),
-	.recieve_st_valid       (eth_net_recieve_valid),
-	.recieve_st_sop         (eth_net_recieve_startofpacket),
-	.recieve_st_eop         (eth_net_recieve_endofpacket),
-	.recieve_st_empty       (eth_net_recieve_empty),
-	.recieve_st_ready       (eth_net_recieve_ready),
-	.transmit_st_data       (eth_net_transmit_data),
-	.transmit_st_valid      (eth_net_transmit_valid),
-	.transmit_st_sop        (eth_net_transmit_startofpacket),
-	.transmit_st_eop        (eth_net_transmit_endofpacket),
-	.transmit_st_empty      (eth_net_transmit_empty),
-	.transmit_st_ready      (eth_net_transmit_ready)
+	.clk                    (clk_clk                  ),
+	.rst_n                  (PB_RESETn                ),
+	.mm_master_address      (mm_out_address           ),
+	.mm_master_write        (mm_out_write             ),
+	.mm_master_writedata    (mm_out_writedata         ),
+	.mm_master_read         (mm_out_read              ),
+	.mm_master_readdatavalid(mm_out_readdatavalid     ),
+	.mm_master_readdata     (mm_out_readdata          ),
+	.mm_master_waitrequest  (mm_out_waitrequest       ),
+	.recieve_st_data        (recieve_out_data         ),
+	.recieve_st_valid       (recieve_out_valid        ),
+	.recieve_st_sop         (recieve_out_startofpacket),
+	.recieve_st_eop         (recieve_out_endofpacket  ),
+	.recieve_st_empty       (recieve_out_empty        ),
+	.recieve_st_ready       (recieve_out_ready        ),
+	.transmit_st_data       (transmit_in_data         ),
+	.transmit_st_valid      (transmit_in_valid        ),
+	.transmit_st_sop        (transmit_in_startofpacket),
+	.transmit_st_eop        (transmit_in_endofpacket  ),
+	.transmit_st_empty      (transmit_in_empty        ),
+	.transmit_st_ready      (transmit_in_ready        ),
+	.debug_led              (PLL_LOCKED               )
 );
+
 
 // {ALTERA_IO_END} DO NOT REMOVE THIS LINE!
 // {ALTERA_MODULE_BEGIN} DO NOT REMOVE THIS LINE!
