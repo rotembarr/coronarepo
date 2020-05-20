0868037a3300d1f7a2f0ca636905ff973a2efac0
Bar Haran <barharan222@gmail.com>
2020-05-20 11:13:20 +0300

    Add testbench shell

---
+++ projects/aes/tb/aes_top_tb.sv
@@ -0,0 +1,10 @@
+// As the true top module contains QSYS files, and its problematic
+// to simulate them, this tb is to simulate the top without the
+// AES module. This testbench should be updated manually
+
+
+module aes_top_tb ();
+
+
+
+endmodule
